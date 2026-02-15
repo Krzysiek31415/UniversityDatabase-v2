63ccbbd5f69729c41feec3b9eece04a40d902647
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2026-02-15 02:28:56 +0100

    refactor: split Person.hpp into hpp and cpp

---
+++ src/domain/Person.cpp
@@ -0,0 +1,34 @@
+#include "Person.hpp"
+
+
+Person::Person(const std::string& name,const std::string& surname, const std::string& PESEL): 
+    name_{std::move(name)}, 
+    surname_{std::move(surname)}, 
+    PESEL_{std::move(PESEL)}
+{}
+const std::string& Person::name() const { return name_; }
+
+const std::string& Person::surname() const { return surname_; }
+
+const std::string& Person::PESEL() const { return PESEL_; }
+
+void Person::addRole(std::unique_ptr<IRole> role){
+    roles_.push_back(std::move(role));
+}
+void Person::setAddress(const std::string& address){
+    address_ = std::move(address);
+}
+const std::optional<std::string> Person::address() const {
+    return address_;
+}
+
+std::vector<IRole*> Person::getRoles() const{
+    std::vector<IRole*> result;
+    result.reserve(roles_.size());
+    for(const auto& r: roles_){
+        result.push_back(r.get());
+    }
+    return result;
+}
+
+
--- src/domain/Person.hpp
+++ src/domain/Person.hpp
@@ -12,23 +12,13 @@
 class Person
 {
 public:
-    Person(const std::string& name,const std::string& surname, const std::string& PESEL): 
-        name_{std::move(name)}, 
-        surname_{std::move(surname)}, 
-        PESEL_{std::move(PESEL)}
-    {}
-    const std::string& name() const { return name_; }
-    const std::string& surname() const { return surname_; }
-    const std::string& PESEL() const { return PESEL_; }
-    void addRole(std::unique_ptr<IRole> role){
-        roles_.push_back(std::move(role));
-    }
-    void setAddress(const std::string& address){
-        address_ = std::move(address);
-    }
-    const std::optional<std::string> address() const {
-        return address_;
-    }
+    Person(const std::string& name,const std::string& surname, const std::string& PESEL);
+    const std::string& name() const;
+    const std::string& surname() const; 
+    const std::string& PESEL() const;
+    void addRole(std::unique_ptr<IRole> role);
+    void setAddress(const std::string& address);
+    const std::optional<std::string> address() const;
 
     template<class T>
     void removeRole(){
@@ -43,14 +33,7 @@ public:
         roles_.end());
     }
 
-    std::vector<IRole*> getRoles() const{
-        std::vector<IRole*> result;
-        result.reserve(roles_.size());
-        for(const auto& r: roles_){
-            result.push_back(r.get());
-        }
-        return result;
-    }
+    std::vector<IRole*> getRoles() const;
 
     template<class T>
     T* getTrait() const {
