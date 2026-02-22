89e4a598d66e946ade2cfecf09b2ef4ee5d80c5b
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2026-02-22 19:27:14 +0100

    test: add unit tests for salary sorting

--- tests/PersonQueryServiceTests.cpp
+++ tests/PersonQueryServiceTests.cpp
@@ -17,7 +17,8 @@ class PersonQueryServiceTest: public ::testing::Test{
             auto p1 = std::make_unique<Person>("Adam", "Smith", "98031715344");
             auto p2 = std::make_unique<Person>("Ralf", "Adams", "78032715494");
             auto p3 = std::make_unique<Person>("John", "Bacon", "88032715314");
-            
+            p1->addRole(std::make_unique<EmployeeRole>(37000));
+            p3->addRole(std::make_unique<EmployeeRole>(70000));
             repo.add(std::move(p1));
             repo.add(std::move(p2));
             repo.add(std::move(p3));
@@ -60,4 +61,29 @@ TEST_F(PersonQueryServiceTest, CanSortDescendingByLastName){
     EXPECT_EQ(result[0]->surname(), "Smith");
     EXPECT_EQ(result[1]->surname(), "Bacon");
     EXPECT_EQ(result[2]->surname(), "Adams");
+}
+
+TEST_F(PersonQueryServiceTest, CanSortAscendingBySalary){
+    auto result = service->sortBySalary(SortOrder::Asc);
+    ASSERT_EQ(result.size(), 3);
+
+    ASSERT_TRUE(result[0]->salary().has_value());
+    ASSERT_TRUE(result[1]->salary().has_value());
+    ASSERT_FALSE(result[2]->salary().has_value());
+
+    EXPECT_DOUBLE_EQ(result[0]->salary().value(), 37000.0);
+    EXPECT_DOUBLE_EQ(result[1]->salary().value(), 70000.0);
+
+}
+
+TEST_F(PersonQueryServiceTest, CanSortDescendingBySalary){
+    auto result = service->sortBySalary(SortOrder::Desc);
+    ASSERT_EQ(result.size(), 3);
+
+    ASSERT_TRUE(result[0]->salary().has_value());
+    ASSERT_TRUE(result[1]->salary().has_value());
+    ASSERT_FALSE(result[2]->salary().has_value());
+
+    EXPECT_DOUBLE_EQ(result[0]->salary().value(), 70000.0);
+    EXPECT_DOUBLE_EQ(result[1]->salary().value(), 37000.0);
 }
