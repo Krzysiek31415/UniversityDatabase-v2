e0322f63899b92e0d07975471225f60b5a972250
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2026-02-24 21:40:12 +0100

    test: add unit tests for index number sorting

--- tests/PersonQueryServiceTests.cpp
+++ tests/PersonQueryServiceTests.cpp
@@ -19,6 +19,8 @@ class PersonQueryServiceTest: public ::testing::Test{
             auto p3 = std::make_unique<Person>("John", "Bacon", "88032715314");
             p1->addRole(std::make_unique<EmployeeRole>(37000));
             p3->addRole(std::make_unique<EmployeeRole>(70000));
+            p1->addRole(std::make_unique<StudentRole>("182696"));
+            p3->addRole(std::make_unique<StudentRole>("201332"));
             repo.add(std::move(p1));
             repo.add(std::move(p2));
             repo.add(std::move(p3));
@@ -86,4 +88,29 @@ TEST_F(PersonQueryServiceTest, CanSortDescendingBySalary){
 
     EXPECT_DOUBLE_EQ(result[0]->salary().value(), 70000.0);
     EXPECT_DOUBLE_EQ(result[1]->salary().value(), 37000.0);
+}
+
+TEST_F(PersonQueryServiceTest, CanSortAscendingByIndexNumber){
+    auto result = service->sortByIndex(SortOrder::Asc);
+    ASSERT_EQ(result.size(), 3);
+
+    ASSERT_TRUE(result[0]->index().has_value());
+    ASSERT_TRUE(result[1]->index().has_value());
+    ASSERT_FALSE(result[2]->index().has_value());
+
+    EXPECT_EQ(result[0]->index().value(), "182696");
+    EXPECT_EQ(result[1]->index().value(), "201332");
+
+}
+
+TEST_F(PersonQueryServiceTest, CanSortDescendingByIndexNumber){
+    auto result = service->sortByIndex(SortOrder::Desc);
+    ASSERT_EQ(result.size(), 3);
+
+    ASSERT_TRUE(result[0]->index().has_value());
+    ASSERT_TRUE(result[1]->index().has_value());
+    ASSERT_FALSE(result[2]->index().has_value());
+
+    EXPECT_EQ(result[0]->index().value(), "201332");
+    EXPECT_EQ(result[1]->index().value(), "182696");
 }
