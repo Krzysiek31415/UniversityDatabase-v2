5c7b578557babbc48f6bfb0b93a8dd8d143d8598
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2026-02-28 18:23:51 +0100

    test: add unit tests for findBySurname function

--- tests/InMemoryPersonRepository_tests.cpp
+++ tests/InMemoryPersonRepository_tests.cpp
@@ -4,6 +4,7 @@
 #include "domain/EmployeeRole.hpp"
 #include "repository/infrastructure/InMemoryPersonRepository.hpp"
 #include <memory>
+#include <set>
 
 TEST(InMemoryPersonRepositoryTest, AddAndGetAllPersons){
 
@@ -33,7 +34,7 @@ class InMemoryPersonRepositoryTestF: public ::testing::Test{
 protected:
     InMemoryPersonRepository repo; 
     void SetUp() override{
-        auto p1 = std::make_unique<Person>("Adam", "Smith", "98031715344");
+        auto p1 = std::make_unique<Person>("Adam", "Smithson", "98031715344");
         auto p2 = std::make_unique<Person>("Ralf", "Smith", "88032715314");
         repo.add(std::move(p1));
         repo.add(std::move(p2));
@@ -61,4 +62,26 @@ TEST_F(InMemoryPersonRepositoryTestF, FindPersonByPESEL){
     EXPECT_EQ(person->PESEL(), "88032715314");
 }
 
+TEST_F(InMemoryPersonRepositoryTestF, CanFindPersonByUniqueSurname){
+    auto result = repo.findBySurname("Smith");
+
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result.front()->surname(), "Smith");
+}
+
+TEST_F(InMemoryPersonRepositoryTestF, CanFindSomePeopleBySameSurname){
+    auto p = std::make_unique<Person>("Paul", "Smith", "91032715314");
+    repo.add(std::move(p));
+
+    auto result = repo.findBySurname("Smith");
+
+    ASSERT_EQ(result.size(), 2);
+
+    std::set<std::string> pesels;
+    for(const auto* person : result)
+        pesels.insert(person->PESEL());
+
+    EXPECT_TRUE(pesels.contains("88032715314"));
+    EXPECT_TRUE(pesels.contains("91032715314"));
+}
 
